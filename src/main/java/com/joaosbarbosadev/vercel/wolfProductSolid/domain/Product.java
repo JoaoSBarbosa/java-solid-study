b90afa896da1d2b63ef20939dd2e53d5deed3ea0
João S. Barbosa <contato.jsbarbosa@gmail.com>
2025-05-11 13:32:12 -0300

    :sparkles: feat: Implementado entidade Produto

---
+++ src/main/java/com/joaosbarbosadev/vercel/wolfProductSolid/domain/Product.java
@@ -0,0 +1,25 @@
+package com.joaosbarbosadev.vercel.wolfProductSolid.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table( name = "tb_produto")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Product {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome")
+    private String name;
+    @Column(name = "preco")
+    private Double price;
+    @Column(name = "estoque")
+    private Integer stock;
+}
