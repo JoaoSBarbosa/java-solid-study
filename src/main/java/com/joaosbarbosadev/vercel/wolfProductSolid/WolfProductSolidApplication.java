1a938b33651dc830bcb21a8f5f3f39a69745ec48
João S. Barbosa <contato.jsbarbosa@gmail.com>
2025-05-11 13:07:44 -0300

    :tada: Iniciando estudos em SOLID

---
+++ src/main/java/com/joaosbarbosadev/vercel/wolfProductSolid/WolfProductSolidApplication.java
@@ -0,0 +1,13 @@
+package com.joaosbarbosadev.vercel.wolfProductSolid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolfProductSolidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolfProductSolidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaosbarbosadev/vercel/wolfProductSolid/WolfProductSolidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaosbarbosadev.vercel.wolfProductSolid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolfProductSolidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
